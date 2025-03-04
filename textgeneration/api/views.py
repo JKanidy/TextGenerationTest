c2abf3e80e2f28c382230e78cd724ac6331124b3
jorak <jorakanidi@gmail.com>
2025-03-04 15:09:49 +0500

    сделал все в докере

--- textgeneration/api/views.py
+++ textgeneration/api/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserInputForm
-
+from django.http import HttpResponse
 # Create your views here.
 def api(request):
-    if request.POST:
-        form = UserInputForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('api')
-    return render(request, 'textgeneration-app/public/api.html', {'form': UserInputForm})
+    return HttpResponse("<h1>Main</h1>")
 
 def about(request):
-    return render(request, 'textgeneration-app/public/api.html')
+    return HttpResponse("<h1>About</h1>")
